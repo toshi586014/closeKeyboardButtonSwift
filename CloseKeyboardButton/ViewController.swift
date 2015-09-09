6a01ea7830f0224ff614023da1244fc3022965ef
3BR-T <3br-t@3BR-T.local>
2015-09-09 12:24:31 +0900

    Initial Commit

---
+++ CloseKeyboardButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloseKeyboardButton
+//
+//  Created by 3BR-T on 2015/09/09.
+//  Copyright (c) 2015年 3BR-T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
